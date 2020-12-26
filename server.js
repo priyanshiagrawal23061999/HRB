9464c03be266b317b4de4517db828f0571a5a8d3
Pawan08082000 <59208833+Pawan08082000@users.noreply.github.com>
2020-12-26 21:17:19 +0530

    Basic Project Setup

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const chalk = require("chalk");
+
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(morgan('dev'));
+
+var server = app.listen(9000, function(){
+    var port = server.address().port;
+    console.log(chalk.bgGreen('Server is running on port',port));
+});
