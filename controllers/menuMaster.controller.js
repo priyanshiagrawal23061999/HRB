bdbf39873f9db4164b74b327cce355d86edc322e
Priyanshi Agrawal <priyanshiagrawalccs@gmail.com>
2021-02-20 13:04:31 +0530

    PA - Empty fields Validations

--- controllers/menuMaster.controller.js
+++ controllers/menuMaster.controller.js
@@ -18,6 +18,26 @@ exports.getEmployees = (req, res) =>{
 }
 
 exports.insertEmployee = (req, res) =>{
+    console.log(req.body)
+    if((req.body.EmployeeName==null || req.body.EmployeeName=='') || 
+    (req.body.Company==null || req.body.Company=='' )||
+        (req.body.Department==null || req.body.Department=='') || 
+        (req.body.Designation==null || req.body.Designation=='') ||
+        (req.body.Email==null || req.body.Email=='') || 
+        (req.body.JoiningDate == null || req.body.JoiningDate == '') ||
+        (req.body.ReportingTo==null || req.body.ReportingTo=='') || 
+        (req.body.DOB==null || req.body.DOB=='') ||
+        (req.body.WorkType==null || req.body.WorkType=='') || 
+        (req.body.EmploymentType==null || req.body.EmploymentType=='') ||
+        (req.body.OfficeBranch==null || req.body.OfficeBranch=='') || 
+        (req.body.EmployeeGrade==null || req.body.EmployeeGrade=='') ||
+        (req.body.EmployeeGroup==null || req.body.EmployeeGroup=='') || 
+        (req.body.EmployeeType==null || req.body.EmployeeType=='') ||
+        (req.body.Value==null || req.body.Value=='') || 
+        (req.body.EffectiveDate==null || req.body.EffectiveDate=='')){
+            res.status(422).send('Fill all the required fields')
+        }
+        else{
     emplId = ""
     Employees.countDocuments({Company:{$gte: req.body.Company}}, function (err, count) {
         emplId = `0601${req.body.Company}${count+1}`
@@ -34,6 +54,7 @@ exports.insertEmployee = (req, res) =>{
         })
       });
      
+    }
     
 }
 
