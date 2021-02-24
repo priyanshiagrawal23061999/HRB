b4a080e5c79f0a0cee5e5e6982eeca8fe3cb4f1d
Pawan08082000 <59208833+Pawan08082000@users.noreply.github.com>
2021-02-24 14:24:04 +0530

    verifyToken + insertEmployee

--- controllers/menuMaster.controller.js
+++ controllers/menuMaster.controller.js
@@ -1,11 +1,12 @@
 const db = require("../models");
+const mongoose = require("mongoose");
 
 
 const Employees = db.employees;
+const User = db.user;
 
 
 exports.getEmployees = (req, res) =>{
-    console.log("show employees for smart table")
     Employees.find({}, function(err, users) {
         var userMap = {};
     
@@ -18,45 +19,78 @@ exports.getEmployees = (req, res) =>{
 }
 
 exports.insertEmployee = (req, res) =>{
-    console.log(req.body)
-    if((req.body.EmployeeName==null || req.body.EmployeeName=='') || 
-    (req.body.Company==null || req.body.Company=='' )||
-        (req.body.Department==null || req.body.Department=='') || 
-        (req.body.Designation==null || req.body.Designation=='') ||
-        (req.body.Email==null || req.body.Email=='') || 
-        (req.body.JoiningDate == null || req.body.JoiningDate == '') ||
-        (req.body.ReportingTo==null || req.body.ReportingTo=='') || 
-        (req.body.DOB==null || req.body.DOB=='') ||
-        (req.body.WorkType==null || req.body.WorkType=='') || 
-        (req.body.EmploymentType==null || req.body.EmploymentType=='') ||
-        (req.body.OfficeBranch==null || req.body.OfficeBranch=='') || 
-        (req.body.EmployeeGrade==null || req.body.EmployeeGrade=='') ||
-        (req.body.EmployeeGroup==null || req.body.EmployeeGroup=='') || 
-        (req.body.EmployeeType==null || req.body.EmployeeType=='') ||
-        (req.body.Value==null || req.body.Value=='') || 
-        (req.body.EffectiveDate==null || req.body.EffectiveDate=='')){
-            res.status(422).send('Fill all the required fields')
-        }
-        else{
-    emplId = ""
-    Employees.countDocuments({Company:{$gte: req.body.Company}}, function (err, count) {
-        emplId = `0601${req.body.Company}${count+1}`
-        req.body.EmployeeId = emplId
-        
-        Employees.create(req.body, function(err, data){ 
-            if(err){
-                console.log('error')
-                res.status(500).send(err)
-            }
-            else{
-            res.send('inserted') // Success 
-            }
-        })
-      });
-     
-    }
+    const employee = new Employees(req.body);
+    User.findOne({_id: mongoose.Types.ObjectId(req.userId) },  (err, user) => {
+        if (user) {
+            employee.User.push(user);
+            Employees.findOne({User: mongoose.Types.ObjectId(req.userId) },  (err, emp) =>{
+                if (!emp){
+            // The below two lines will add the user's 
+            // ObjectID to the the employee's User array field
+            employee.save().then(result =>{
+                return res.status(200).send({ message: 'Employee created!' })
+            }).catch(err=>{
+
+                return res.status(500).send(err)
+            })
+                }
+                if (emp){
+                    Employees.updateOne({User: mongoose.Types.ObjectId(req.userId) }, req.body, (err, success)=>{
+                        if (err){
+                            return res.status(500).send(err.message)
+                        }
+                        if (success){
+                            return res.status(204).send({ message: 'Employee Updated!' })
     
+                        }
+                    })
+                }
+            })
+            
+        }
+        if (err) return res.status(500).send({message: err.err.message})
+    });
 }
+// exports.insertEmployee = (req, res) =>{
+//     console.log(req.body)
+//     if((req.body.EmployeeName==null || req.body.EmployeeName=='') || 
+//     (req.body.Company==null || req.body.Company=='' )||
+//         (req.body.Department==null || req.body.Department=='') || 
+//         (req.body.Designation==null || req.body.Designation=='') ||
+//         (req.body.Email==null || req.body.Email=='') || 
+//         (req.body.JoiningDate == null || req.body.JoiningDate == '') ||
+//         (req.body.ReportingTo==null || req.body.ReportingTo=='') || 
+//         (req.body.DOB==null || req.body.DOB=='') ||
+//         (req.body.WorkType==null || req.body.WorkType=='') || 
+//         (req.body.EmploymentType==null || req.body.EmploymentType=='') ||
+//         (req.body.OfficeBranch==null || req.body.OfficeBranch=='') || 
+//         (req.body.EmployeeGrade==null || req.body.EmployeeGrade=='') ||
+//         (req.body.EmployeeGroup==null || req.body.EmployeeGroup=='') || 
+//         (req.body.EmployeeType==null || req.body.EmployeeType=='') ||
+//         (req.body.Value==null || req.body.Value=='') || 
+//         (req.body.EffectiveDate==null || req.body.EffectiveDate=='')){
+//             res.status(422).send('Fill all the required fields')
+//         }
+//         else{
+//     emplId = ""
+//     Employees.countDocuments({Company:{$gte: req.body.Company}}, function (err, count) {
+//         emplId = `0601${req.body.Company}${count+1}`
+//         req.body.EmployeeId = emplId
+        
+//         Employees.create(req.body, function(err, data){ 
+//             if(err){
+//                 console.log('error')
+//                 res.status(500).send(err)
+//             }
+//             else{
+//             res.send('inserted') // Success 
+//             }
+//         })
+//       });
+     
+//     }
+    
+// }
 
 exports.searchEmployee = (req, res) => {
     var query = req.params.query;
