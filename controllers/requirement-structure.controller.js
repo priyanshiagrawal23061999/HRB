1e5b68724478f8a70128e112d23e5f85e09dc387
Priyanshi Agrawal <priyanshiagrawalccs@gmail.com>
2021-03-11 19:30:45 +0530

    PA - add job vacancy api correction

--- controllers/requirement-structure.controller.js
+++ controllers/requirement-structure.controller.js
@@ -6,7 +6,7 @@ const JobVacancy = db.jobVacancy;
 exports.insertJobVacancy = (req, res) =>{
     console.log(req.body)
     JobVacancy.insertMany(req.body).then(function(){ 
-        res.send('inserted') // Success 
+        res.send({message: 'inserted'}) // Success 
     }).catch(function(error){ 
         console.log(error)      // Failure 
     }); 
